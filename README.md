d73137d9105efa68291fd9d6336961e726b8bf07
Alyx DARENNE <axda.web@gmail.com>
2023-08-08 15:04:12 +0200

    Update README + Add project screenshot

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
-# Démarrer l'application en local:
+# Daily Quotes
 
-1. `npm install`
-2. `npm run server`
-3. `npm start`
+[![React.js](https://img.shields.io/badge/React.js-18.1.0-61DAFB?logo=react&logoColor=white&style=flat-square)](https://reactjs.org/)
+[![Styled Components](https://img.shields.io/badge/Styled_Components-5.3.5-%23DB7093?logo=styled-components&logoColor=white&style=flat-square)](https://styled-components.com/)
+
+![screenshot](/screenshot.jpg)
+
+## Description
+
+Single Page Application displaying quotes dynamically thanks to the use of an API. It also allows to tweet the chosen quote on its Twitter account. Use of the Styled-components library for the formatting.
+
+This project explores various concepts and features including:
+
+- React State
+- CSS in JS
+- CSS Animations
+- ES6
+- REST API
+
+and more.
+
+## Technologies Used
+
+- [React.js](https://reactjs.org/) - Version 18.1.0
+- [Styled Components](https://styled-components.com/) - Version 5.3.5
+
+## Getting Started
+
+To get started with the Daily Quotes project, follow the steps below:
+
+1. Clone the repository:
+
+   ```shell
+   git clone git@github.com:Axda-Web/dailyQuotes.git
+   ```
+
+2. Change into the project directory:
+
+   ```shell
+   cd dailyQuotes
+   ```
+
+3. Install the project dependencies:
+
+   ```shell
+   npm install
+   ```
+
+4. Run the project in development mode:
+
+   ```shell
+   npm start
+   ```
+
+A deployed version of the Daily Quotes project is available on Vercel at [Daily-Quotes](https://daily-quotes-beryl.vercel.app/).
--- src/components/slideshow/Slideshow.styled.js
+++ src/components/slideshow/Slideshow.styled.js
@@ -1,44 +1,45 @@
 import styled from "styled-components";
 
 const StyledSlideshow = styled.div`
-    height: 250px;
-    background: linear-gradient(0deg, rgba(0, 0, 0, .2), rgba(0, 0, 0, 0)), url(${({backgroundImgUrl}) => backgroundImgUrl});
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    border-radius: 10px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px;
+  height: 250px;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0)),
+    url(${({ backgroundImgUrl }) => backgroundImgUrl});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+
+  .arrow {
+    cursor: pointer;
+    height: 25px;
+    transition: transform 0.2s;
+    transform: scale(1);
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    height: 300px;
+    padding: 20px;
 
     .arrow {
-        cursor: pointer;
-        height: 25px;
-        transition: transform .2s;
-        transform: scale(1);
-
-        &:hover {
-            transform: scale(1.05);
-        }
+      min-height: 40px;
     }
+  }
 
-    @media (min-width: ${({theme}) => theme.breakpoints.mobile}) {
-       height: 300px;
-       padding: 20px;
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    height: 350px;
 
-       .arrow {
-        min-height: 40px;
-       }
+    .arrow {
+      min-height: 50px;
     }
+  }
+`;
 
-    @media (min-width: ${({theme}) => theme.breakpoints.desktop}) {
-       height: 350px;
-
-       .arrow {
-        min-height: 50px;
-       }
-    }
-`
-
-export default StyledSlideshow
+export default StyledSlideshow;
