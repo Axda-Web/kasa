573f14e414e77ee922ecf3ff956c0f1d60199824
Alyx DARENNE <axda.web@gmail.com>
2023-08-08 10:44:30 +0200

    Fix: Update page layout

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 //Components
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 //Pages
-import Home from '../pages/home'
-import Housing from '../pages/housing'
-import About from '../pages/about'
-import Error from '../pages/error'
+import Home from "../pages/home";
+import Housing from "../pages/housing";
+import About from "../pages/about";
+import Error from "../pages/error";
 
-
+const StyledPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+`;
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/housing' element={<Housing />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-      <Footer />
+      <StyledPageContainer>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/housing" element={<Housing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </StyledPageContainer>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
