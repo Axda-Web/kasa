04585bcb9d84712a431822f0ee99d8a54c42fd3b
Alyx DARENNE <axda.web@gmail.com>
2023-08-08 10:44:17 +0200

    Fix: Bypass json-server for deployment

--- src/components/cardsList/CardsList.jsx
+++ src/components/cardsList/CardsList.jsx
@@ -1,38 +1,35 @@
-import React, { useState, useEffect, useContext } from 'react'
-import StyledCardsList from './CardsList.styled'
-import { v4 as uuidv4 } from 'uuid'
+import React, { useState, useEffect, useContext } from "react";
+import StyledCardsList from "./CardsList.styled";
+import { v4 as uuidv4 } from "uuid";
+import jsonData from "../../db.json";
 
-import { HousingContext } from '../../context/housingContext'
+import { HousingContext } from "../../context/housingContext";
 
-
-import Card from '../card'
+import Card from "../card";
 
 const CardsList = () => {
+  const [data, setData] = useState([]);
 
-  const [data, setData] = useState([])
-
-  useEffect( () => {
+  useEffect(() => {
     const getData = async () => {
-      const res = await fetch('http://localhost:3001/data')
-      const json = await res.json()
-      setData(json)
-    }
-    getData()
-  }, [])
+      // const res = await fetch('http://localhost:3001/data')
+      // const json = await res.json();
+      // setData(json);
+      setData(jsonData.data);
+    };
+    getData();
+  }, [data]);
 
-  const { setHousing } = useContext(HousingContext)
-  setHousing(data)
+  const { setHousing } = useContext(HousingContext);
+  setHousing(data);
 
-  const cards = data.map( card => <Card key={uuidv4()} {...card} />)
-
+  const cards = data.map((card) => <Card key={uuidv4()} {...card} />);
 
   return (
     <StyledCardsList>
-      <div className='container'>
-        {cards}
-      </div>
+      <div className="container">{cards}</div>
     </StyledCardsList>
-  )
-}
+  );
+};
 
-export default CardsList
+export default CardsList;
